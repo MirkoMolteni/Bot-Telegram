346d498c77d9350db5fd9e5ac4f55c1d783ad0b7
MirkoMolteni <mirko.molteni.sc@gmail.com>
2023-12-16 11:35:30 +0100

    Inizio bot telegram

---
+++ main.py
@@ -0,0 +1,20 @@
+import requests
+from time import sleep
+
+file = open("token_bot.txt", "r")
+token = file.read()
+
+last_update_id = 0
+while True:
+    ENDPOINT = f'https://api.telegram.org/{token}/'
+    method = "getUpdates"
+    parametri = {'offset': 0}
+
+    url = ENDPOINT + method
+    response = requests.get(url, params=parametri)
+    data = response.json()
+    #elaboro i dati
+    #last_update_id = ... + 1
+    print(data)
+
+    sleep(5)
